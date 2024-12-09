8539f077c3b4fda3265319218ef1ca9c382f1d04
calebwest27 <westc@jbu.edu>
2024-12-09 09:23:26 -0600

    Add files via upload

--- Gym_Menu.py
+++ Gym_Menu.py
@@ -1,8 +1,8 @@
 import Gym_setup as GS
+import pickle
 import random
 
-# Joel why do we have these definitions in the menu? Not because I think it's wrong I would just think that it's
-# supposed to be on the setup page. But I won't change it because it's fine
+
 def silver():
     print(" ")
     print("1. Silver")
@@ -16,7 +16,7 @@ def gold():
     print("2. Gold")
     print("Starting Fee: $21")
     print("Payment Plan: $50/month")
-    print("Accessible Equipment: Locker Rooms, Showers, Cardio, Pool + Swim Lanes")
+    print("Accessible Equipment: Locker Rooms, Showers, Cardio, Pool and Swim Lanes")
     print(" ")
 
 
@@ -24,15 +24,16 @@ def plat():
     print("3. Platinum")
     print("Starting Fee: $28")
     print("Payment Plan: $65/month")
-    print("Accessible Equipment: Locker Rooms, Showers, Cardio, Pool + Swim Lanes, Resistance / Weight Machines")
+    print("Accessible Equipment: Locker Rooms, Showers, Cardio, Pool and Swim Lanes, Resistance and Weight Machines")
     print(" ")
 
 
 def main_menu():
+    print(" ")
     print("1. Become a Member")
     print("2. Display Members")
     print("3. Remove Member")
-    print("4. Exit Program")
+    print("0. Exit Program")
 
 
 def remove_menu():
@@ -47,12 +48,8 @@ memberPlatList = []
 memberInfo = []
 memberBilling = []
 
-# ====================================================== this is where the code actually begins ===================== #
-print(" ")
-print("Created by: Joel Leone and Caleb West")
-print(" ")
-print("Welcome to the Lit Fitness database! Below is a menu for options to select.")
-print("We thank you for choosing Lit Fitness.")
+print("Welcome to Lit Fitness! Below is a Menu with options to select.")
+print("Thank You For Choosing Lit Fitness.")
 print(" ")
 while 99:
     main_menu()
@@ -62,91 +59,144 @@ while 99:
         gold()
         plat()
         print("0. Exit to Main Menu")
-        print(" ")
         option1 = int(input("Enter the Desired Membership Level: "))
         print(" ")
 
         if option1 == 1:
             gymId = random.sample(range(10000, 99999), 1)
             sm = GS.Member(input("Name: "), input("Age: "), input("Gender: "), gymId, "Silver")
-            print("New member information added. Your gym ID is: ", gymId)
-            print("The following info will be kept private, and only accessible by you.")
-            memberSilverList.append(sm)
-            info = GS.Sensitive_Info(input("Email: "), input("Phone Number: "),
-                                     input("Name On Credit Card: "), input("Billing Address: "),
-                                     input("Expiration Date: "), input("Credit Card Number: "), input("CVV: "))
-            memberInfo.append(info)
-            pay = info.silver_billing()
-            memberBilling.append(pay)
-            for x in range(0, len(memberSilverList)):  # hey joel what do these codes do===============
+            for x in range(0, len(memberSilverList)):  # This Makes sure there are no matching id numbers=========
                 if x == gymId:
                     new_value = random.sample(range(10000, 99999), 1)
                     memberSilverList(x) == new_value
+                    print("Your new gym ID is: ", new_value)
+            memberSilverList.append(sm)
+            print("New member information added. Your gym ID is: ", gymId)
+            print("The following info will be kept private, and only accessible by you.")
+            info = GS.Sensitive_Info(input("Email: "), input("Phone Number: "),
+                                     input("Name On Card: "), input("Billing Address: "),
+                                     input("Expiration Date: "), input("Credit Card Number: "), input("CVV: "))
+            memberInfo.append(info)
+            pay = info.silver_billing()
+            print("Billing Receipt: ")
+            memberBilling.append(pay)
+            sd = open('Silver_Gym.dat', 'ab')
+            pickle.dump(sm, sd)
+            sd.close()
 
         if option1 == 2:
             gymId = random.sample(range(10000, 99999), 1)
             gm = GS.Member(input("Name: "), input("Age: "), input("Gender: "), gymId, "Gold")
+            for x in range(0, len(memberGoldList)):  # This Makes sure there are no matching id numbers=========
+                if x == gymId:
+                    new_value = random.sample(range(10000, 99999), 1)
+                    memberGoldList(x) == new_value
+                    print("Your new gym ID is: ", new_value)
+            memberGoldList.append(gm)
             print("New member information added. Your gym ID is: ", gymId)
             print("The following info will be kept private, and only accessible by you.")
-            memberGoldList.append(gm)
             info = GS.Sensitive_Info(input("Email: "), input("Phone Number: "),
-                                     input("Name On Credit Card: "), input("Billing Address: "),
+                                     input("Name On Card: "), input("Billing Address: "),
                                      input("Expiration Date: "), input("Credit Card Number: "), input("CVV: "))
             memberInfo.append(info)
             pay = info.gold_billing()
             memberBilling.append(pay)
-            for x in range(0, len(memberGoldList)):  # hey joel what do these codes do===============
-                if x == gymId:
-                    new_value = random.sample(range(10000, 99999), 1)
-                    memberGoldList(x) == new_value
+            gd = open('Gold_Gym.dat', 'ab')
+            pickle.dump(gm, gd)
+            gd.close()
 
         if option1 == 3:
             gymId = random.sample(range(10000, 99999), 1)
             pm = GS.Member(input("Name: "), input("Age: "), input("Gender: "), gymId, "Platinum")
+            for x in range(0, len(memberPlatList)):  # This Makes sure there are no matching id numbers=========
+                if x == gymId:
+                    new_value = random.sample(range(10000, 99999), 1)
+                    memberPlatList(x) == new_value
+                    print("Your new gym ID is: ", new_value)
+            memberPlatList.append(pm)
             print("New member information added. Your gym ID is: ", gymId)
             print("The following info will be kept private, and only accessible by you.")
-            memberPlatList.append(pm)
             info = GS.Sensitive_Info(input("Email: "), input("Phone Number: "),
-                                     input("Name On Credit Card: "), input("Billing Address: "),
+                                     input("Name On Card: "), input("Billing Address: "),
                                      input("Expiration Date: "), input("Credit Card Number: "), input("CVV: "))
             memberInfo.append(info)
             pay = info.plat_billing()
             memberBilling.append(pay)
-            for x in range(0, len(memberPlatList)):  # hey joel what do these codes do====================
-                if x == gymId:
-                    new_value = random.sample(range(10000, 99999), 1)
-                    memberPlatList(x) == new_value
+            pd = open('Plat_Gym.dat', 'ab')
+            pickle.dump(pm, pd)
+            pd.close()
 
         if option1 == 0:
             print("Exiting to Main Menu")
 
+        if option1 > 3:
+            print("Please Enter A Valid Input")
+            print(" ")
+        if option1 < 0:
+            print("Please Enter A Valid Input")
+            print(" ")
+
     if choice == 2:
         while 2:
+            print(" ")
             print("1. Display Silver Members")
             print("2. Display Gold Members")
             print("3. Display Platinum Members")
             print("0. Exit to Main Menu")
             option2 = int(input())
             if option2 == 1:
-                for x in range(0, len(memberSilverList)):
-                    sm.display_member()
+                sd = open('Silver_Gym.dat', 'rb')
+                print("Silver Members: ")
+                while 10:
+                    try:
+                        silver_data = pickle.load(sd)
+                        print(silver_data.name)
+                        print(silver_data.age)
+                        print(silver_data.gender)
+                        print(silver_data.gymID)
+                        print(silver_data.memLevel)
+                    except EOFError:
+                        break
+                sd.close()
             if option2 == 2:
-                for x in range(0, len(memberGoldList)):
-                    gm.display_member()
+                gd = open('Gold_Gym.dat', 'rb')
+                print("Gold Members: ")
+                while 11:
+                    try:
+                        gold_data = pickle.load(gd)
+                        print(gold_data.name)
+                        print(gold_data.age)
+                        print(gold_data.gender)
+                        print(gold_data.gymID)
+                        print(gold_data.memLevel)
+                    except EOFError:
+                        break
+                gd.close()
             if option2 == 3:
-                for x in range(0, len(memberPlatList)):
-                    pm.display_member()
+                pd = open('Plat_Gym.dat', 'rb')
+                print("Platinum Members: ")
+                while 12:
+                    try:
+                        plat_data = pickle.load(pd)
+                        print(plat_data.name)
+                        print(plat_data.age)
+                        print(plat_data.gender)
+                        print(plat_data.gymID)
+                        print(plat_data.memLevel)
+                    except EOFError:
+                        break
+                pd.close()
             if option2 == 0:
                 print("Exiting to Main Menu")
                 break
 
     if choice == 3:
         while 2:
-            print("Which membership (silver / gold / platinum) is your membership?")
+            print("Which membership Level are you? (Silver / Gold / Platinum)")
             print("1. Silver")
             print("2. Gold")
             print("3. Platinum")
-            print("4. Return to Menu")
+            print("0. Return to Menu")
             option3 = int(input())
             if option3 == 1:  # silver
                 user = input("Enter gym ID: ")
@@ -190,12 +240,17 @@ while 99:
                             print("Returning to 'remove' menu.")
                     else:
                         print("ID not found in database.")
-            if option3 == 4:
+            if option3 == 0:
                 print("Returning to Main Menu")
                 print(" ")
                 break
 
-    if choice == 4:
-        print(" ")
-        print("Program terminated. We thank you for your time with Lit Fitness.")
+    if choice == 0:
+        print("Program Terminated. We thank you for your time with Lit Fitness")
         break
+
+    else:
+        print("Please Enter A Valid Input")
+        print(" ")
+
+    # if choice == 4:
